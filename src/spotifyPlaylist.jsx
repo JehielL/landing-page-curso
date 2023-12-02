ee59f12972b467341b6a73f9baa01c9cd68004ed
JehielL <jehiel.py@gmail.com>
2023-12-02 13:21:20 +0100

    add spotify playlist

--- src/spotifyPlaylist.jsx
+++ src/spotifyPlaylist.jsx
@@ -7,7 +7,7 @@ const SpotifyPlaylist = () => {
       src="https://open.spotify.com/embed/playlist/3UYW0xZsL7G0LST04uG4Bt?utm_source=generator"
       width="100%"
       height="352"
-      frameBorder="0"
+
       allowFullScreen=""
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       loading="lazy"
